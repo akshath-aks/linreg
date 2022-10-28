318abb0bad1ede0b001867e7f1a65d4b8effd1ea
Akshath Srinivas <akshath.nirmala@gmail.com>
2022-10-28 19:06:03 +0200

    added libraries

--- R/visualizing_flight_delay.R
+++ R/visualizing_flight_delay.R
@@ -15,7 +15,7 @@ Visualizing_flight_delay<-function(){
   flights<-flights[!is.na(flights$arr_delay),]
   
   #calculating delays of destination airport
-  requireNamespace('dplyr')
+  library(dplyr)
   fl_dest<- flights %>% dplyr::group_by(dest) %>%
     dplyr::summarise(avg_delay_dest=mean(arr_delay+dep_delay))
   colnames(fl_dest)<-c('airports','mean_delay')
@@ -39,7 +39,8 @@ Visualizing_flight_delay<-function(){
   
   
   #hover over the points to see mean delay
-  requireNamespace('ggplot2')
+  library(ggplot2)
+  library(plotly)
   p<-ggplot2::ggplot(merging,ggplot2::aes(label1=mean_delay))+
     ggplot2::geom_point(ggplot2::aes(x=lat,y=lon,color=airport))+
     ggplot2::theme(legend.position = 'none')+xlab('latitude')+ylab('longitude')+
